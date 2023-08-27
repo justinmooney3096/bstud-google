39c518247742e9738c61749892ea65c7c81cdd63
Justin Mooney <justin.patrick.mooney@gmail.com>
2023-08-27 19:13:43 -0400

    added log

--- groupme-api.js
+++ groupme-api.js
@@ -60,7 +60,7 @@ const getMyLikeList = async () => {
         if (response.statusCode == 200) {
             console.log("Hello")
             const likedMessageList = response.body.response.messages
-            console.log("Printing liked messages...\n"+response)
+            console.log("Printing liked messages (response)...\n"+response)
             console.log("Successfully retrieved liked message list")
             return likedMessageList
         }
