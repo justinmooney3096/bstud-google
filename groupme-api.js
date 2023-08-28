7a3345dbf63aee7cfc9264e81eca00898432cb9b
Justin Mooney <justin.patrick.mooney@gmail.com>
2023-08-28 12:17:42 -0400

    commenting destURL variable type

--- groupme-api.js
+++ groupme-api.js
@@ -53,7 +53,7 @@ const getMyLikeList = async () => {
         const myLikePath = `/v3/groups/${groupid}/likes/mine?token=${accesstoken}`
         const destUrl = new URL(myLikePath, baseurl)
         console.log("destURL = "+destUrl)
-        console.log("Type of variable destURL = "|typeof(destUrl))
+        console.log("Type of variable destURL = "+typeof(destUrl))
         const response = await got.get(destUrl).json()
         //const response = await got(destUrl, {
         //    responseType: "json"
